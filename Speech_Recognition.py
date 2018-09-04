1cb8ef3df25b104fe80e281a6b18add2b078f78e
Darshil Shah <shahdarshil_sh@srmuniv.edu.in>
2018-09-04 22:28:18 +0530

    Create Speech_Recognition.py

---
+++ Speech_Recognition.py
@@ -0,0 +1,13 @@
+import speech_recognition as sr
+
+r=sr.Recognizer()
+with sr.Microphone() as source:
+	r.adjust_for_ambient_noise(source)  # here
+	print('Say Something')
+	audio=r.listen(source,timeout=3)
+
+try:
+	print('Answer:\n' + r.recognize_google(audio))
+
+except:
+	pass
